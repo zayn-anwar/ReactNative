eb65e869cce13c341cf54898beb94f1a9bdee1cf
Zayn A. <165022269+zayn-anwar@users.noreply.github.com>
2025-07-01 21:00:27 +0300

    Create contact.jsx

---
+++ RandomWebsite/contact.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Contact = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contact Page</Text>
+      <Link href="/" style={styles.link}>Return Home</Link>
+    </View>
+  )
+}
+
+export default Contact
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontWeight:'bold',
+    fontSize:18
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
+  }
+})
